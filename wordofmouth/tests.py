3b4e0d488eab1916da2e997fac123cc3062f9aaa
zhunoah <zhunoah13@gmail.com>
2022-03-24 09:21:16 -0400

    test case for Recipe model

--- wordofmouth/tests.py
+++ wordofmouth/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
+from .models import Recipe
+from django.utils import timezone
 
 # Create your tests here.
 class LoginTest(TestCase):
@@ -13,3 +15,12 @@ class LoginTest(TestCase):
         response = self.client.post('/accounts/login/', self.credentials, follow=True)
         # should be logged in now
         self.assertEquals(response.status_code, 200)
+
+class RecipeModelTests(TestCase):
+    def test_title_likes_instructions(self):
+        r = Recipe(recipe_title='Bread and Wine', likes=12, pub_date=timezone.now(), instructions='get bread and wine')
+        r.save()
+        self.assertEquals(r.recipe_title, 'Bread and Wine')
+        self.assertEquals(r.likes, 12)
+        self.assertEquals(r.instructions, 'get bread and wine')
+        r.delete()
