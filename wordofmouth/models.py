9e210f0f3f36cb39596aba8464f1b0f36485eea3
HannahTuma <98724712+HannahTuma@users.noreply.github.com>
2022-03-26 20:51:08 -0400

    like button, but it doesn't work properly

--- wordofmouth/models.py
+++ wordofmouth/models.py
@@ -1,16 +1,34 @@
 from urllib import request
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib import auth
 
 # Create your models here.
 
+
+# class User(AbstractUser):
+#     pass
+
+
 class Recipe(models.Model):
     # fields
     recipe_title = models.CharField(max_length=200)
-    likes = models.IntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
     pub_date = models.DateTimeField('date published')
     instructions = models.TextField(blank=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    # user_likes = models.ManyToManyField(User)
 
     # __str__() method to easily see the title
     def __str__(self):
         return self.recipe_title
+
+
+class Like(models.Model):
+    #user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users")
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="recipes")
+    already_liked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"you liked {self.recipe}"
--- wordofmouth/views.py
+++ wordofmouth/views.py
@@ -1,12 +1,18 @@
+from django.urls import reverse
 from django.utils import timezone
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.http import HttpResponse
-from .models import Recipe
+from .models import Recipe, Like
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the word of mouth index.")
+    # return render(request, 'wordofmouth/recipe_detail.html', {
+    #     "recipes": Recipe.objects.all(),
+    #     "likes": Like.objects.all(),
+    # })
+
 
 def recipe_detail(request, recipe_id):
     try:
@@ -30,3 +36,22 @@ def new_recipe(request):
     r = Recipe(recipe_title=request.POST['title'], likes=0, pub_date=timezone.now(), instructions=request.POST['instructions'])
     r.save()
     return render(request, 'wordofmouth/new_recipe.html', {'recipe': r})
+
+
+def like(request, recipe_id):
+    if request.method == "POST":
+        #user = User.objects.get(username=request.user.username)
+        recipe = get_object_or_404(Recipe, pk=recipe_id)
+        #new_like = Like(user=user, recipe=recipe)
+        #new_like.already_liked = True
+        selected_like = recipe.choice_set.get(pk=request.POST['recipe'])
+        selected_like.likes += 1
+        selected_like.save()
+
+        #adds user to post
+        # recipe.user_likes.add(user)
+        # recipe.save()
+        # new_like.save()
+
+        return Recipe.likes
+
