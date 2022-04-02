3fb06c20c203150290f54ea763139ee4d278f6a5
tjvitchutripop <khunteeratham@gmail.com>
2022-04-02 18:52:20 -0400

    Major UI update

--- wordofmouth/urls.py
+++ wordofmouth/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'wordofmouth'
 urlpatterns = [
     path('', views.recipe_explore, name="recipe_explore"),
-    path('experiment', views.recipe_experiment, name="recipe_experiment"),
+    path('create', views.recipe_experiment, name="recipe_experiment"),
     path('recipe/<int:recipe_id>', views.recipe_detail, name="recipe_detail"),
     path('wordofmouth/newrecipe', views.new_recipe, name='new_recipe')
 ]
