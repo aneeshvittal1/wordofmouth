39eed785ac5c68dff79d77ada337fc03a6cb5f3b
Aneesh Vittal <akv6zr@virginia.edu>
2022-05-03 15:32:03 -0400

    static files setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'mysite/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
